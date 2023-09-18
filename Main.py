16f5c36d0c8114994f031fc1772da01c99b68243
alex-sandova1 <alex3sandova@gmail.com>
2023-09-17 19:14:11 -0700

    finished cipher function, more features will be added later

--- Main.py
+++ Main.py
@@ -3,8 +3,15 @@ import datetime
 def C_cipher(messeage, day):
    code = ""
    
+   for ch in messeage:
+      ordvalue = ord(ch) #stores the  value of the character
+      cipherValue = ordvalue + day #stores the new value of the character changing it by the day
+      if cipherValue > ord('z'): #if the new value is greater than the value of z it will wrap around to a
+         cipherValue = ord('a') + day - \
+                       (ord('z') - ordvalue + 1)
+      code += chr(cipherValue) #stores the new character in the code variable
 
-   return code
+   return code #returns the code variable
       
 def main():
    current = datetime.datetime.now()
