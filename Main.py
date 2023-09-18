7b03fea91fb7695605f8fb4d6ae14c94ada8d542
alex-sandova1 <alex3sandova@gmail.com>
2023-09-17 19:07:09 -0700

    started main file and cipher function

---
+++ Main.py
@@ -0,0 +1,20 @@
+import datetime
+
+def C_cipher(messeage, day):
+   code = ""
+   
+
+   return code
+      
+def main():
+   current = datetime.datetime.now()
+   day = current.day #stores the day
+
+   messeage = input("Enter your messeage to be encoded: ")
+      
+   print (C_cipher(messeage, day))
+
+      
+
+
+main()
