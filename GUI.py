91bbd0cb68eaf6f2b7b541e5dd0df5b686e768d8
alex-sandova1 <alex3sandova@gmail.com>
2023-09-23 18:42:37 -0700

    changed to single window to make it easier to use and also able to code and decode in same window

--- GUI.py
+++ GUI.py
@@ -2,86 +2,53 @@ import datetime
 import tkinter as tk
 import coder_decoder
 
-def coder_window():
-    def on_click():
-        current = datetime.datetime.now()
-        day = current.day #stores the day
-        input_ = entry.get()
-        result.config(text=coder_decoder.coder(f"{input_}",day))
-
-    #window is created
-    window = tk.Tk()
-    window.title('coder')
-    window.geometry('500x500')
-
-    #creates a label
-    label = tk.Label(window, text ="Enter Message")
-    label.pack()
-
-    #creates the text box
-    entry = tk.Entry(window)
-    entry.pack()
-
-    button = tk.Button(window, text = "Send", command=on_click)
-    button.pack()
-
-
-    result = tk.Label(window, text="")
-    result.pack()
-    window.mainloop()
-
-def decoder_window():
-
-    def on_click():
-        current = datetime.datetime.now()
-        day = current.day #stores the day
-        input_ = entry.get()
-        result.config(text=coder_decoder.decoder(f"{input_}",day))
-
-    #window is created
-    window = tk.Tk()
-    window.title('decoder')
-    window.geometry('200x200')
-
-    #creates a label
-    label = tk.Label(window, text ="Enter Message")
-    label.pack()
-
-    #creates the text box
-    entry = tk.Entry(window)
-    entry.pack()
-
-    button = tk.Button(window, text = "Send", command=on_click)
-    button.pack()
-
-
-    result = tk.Label(window, text="")
-    result.pack()
-    window.mainloop()
-
 def start_window():
+
+    def code(key):
+
+
+        #encodes the message amd displays in a text box under buttons
+        message = entry.get()
+        coded_message = coder_decoder.coder(message, key)
+        label = tk.Label(window, text = coded_message)
+        label.place(x=150, y=100)
+    
+    def decode(key):
+        #decodes the message amd displays in a text box under buttons
+        message = entry.get()
+        decoded_message = coder_decoder.decoder(message, key)
+        label = tk.Label(window, text = decoded_message)
+        label.place(x=150, y=100)
+
+
 
     #window is created
     window = tk.Tk()
     window.title('Start window')
-    window.geometry('400x400')
+    window.geometry('400x200')
 
     #creates a label
-    label = tk.Label(window, text ="Make a choice")
+    label = tk.Label(window, text ="encrypt or decrypt a message")
     label.place(x=200, y=10, anchor="center")
+
+    label = tk.Label(window, text = "Enter key:")
+
+    #creates the text box
+    entry = tk.Entry(window)
+    entry.place(x= 100, y=20)
 
 
     #create code button
-    code_button = tk.Button(window, text = "Code", command=coder_window)
+    code_button = tk.Button(window, text = "Code", command = code)
     code_button.place(x=100, y=50)
 
     #create decode button
-    decode_button = tk.Button(window, text = "Decode", command=decoder_window)
+    decode_button = tk.Button(window, text = "Decode", command = decode )
     decode_button.place(x=200, y=50)
 
     #create close button
     close_button = tk.Button(window, text = "Close", command= window.destroy)
-    close_button.place(x=155, y=100)
+    close_button.place(x=150, y=160)
 
     window.mainloop()
 
